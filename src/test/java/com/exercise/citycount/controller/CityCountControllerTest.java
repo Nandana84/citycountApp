f518a9065ebbb211469b3e4d0483c0b0096e74e8
Nandana84 <nandana.sree@gmail.com>
2025-04-22 11:33:50 +0100

    Amended the test file name

--- src/test/java/com/exercise/citycount/controller/CityCountControllerTest.java
+++ src/test/java/com/exercise/citycount/controller/CityCountControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class WeatherControllerTest {
+public class CityCountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
